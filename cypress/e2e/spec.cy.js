7b4a742fd37395fdd3a0966ef0a0154d37da23de
siddharth <mksiddhu2@gmail.com>
2024-03-26 12:03:09 +0530

    Modified spec file to fix error

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,10 +2,16 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://google.com')
     cy.get('.SDkEP').type('javascript')
-    cy.get('.G43f7e > li').then(ele =>{
-      cy.log(ele)
-      let text = "Javascript basics"
-      cy.get(`li[data-entityname=${text}]`)
+    cy.get('.G43f7e > li > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)').each(ele =>{
+      // cy.log(ele)
+      let textSearch = "javascript basics"
+      cy.log(ele.text())
+      // let elementText = ele.text.innerHTML
+      if(ele.text().includes(textSearch))
+      {
+        ele.trigger('click')
+      }
+
     })
   })
 })
